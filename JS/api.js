3fc6103e66aaf668eb9fa30ea60069f2f3fac275
OsniaaK <osniaak13@gmail.com>
2024-10-25 13:56:55 -0300

    ID System Fixed

--- JS/api.js
+++ JS/api.js
@@ -1,14 +1,16 @@
-export const apiURL = "https://pokeapi.co/api/v2/pokemon"
+export const apiURL = "https://pokeapi.co/api/v2/pokemon";
 
 export const getOne = async (number = 1) => {
-    try {
-        const query = await fetch(`${apiURL}/${number}/`)
-        const datos = await query.json()
-
-        let {id, forms, abilities, sprites, weight, height, types} = datos
-        return ({id, forms, abilities, sprites, weight, height, types});
-
-    } catch (error) {
-        throw new Error(error.message)
+  try {
+    const query = await fetch(`${apiURL}/${number}/`);
+    const datos = await query.json();
+    if (!query.ok || !datos.id) {
+      throw new Error("Pokémon no encontrado");
     }
-}
+
+    let { id, forms, abilities, sprites, weight, height, types } = datos;
+    return { id, forms, abilities, sprites, weight, height, types };
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
--- app.js
+++ app.js
@@ -5,47 +5,38 @@ const inputNumber = select("#searchPokemon");
 const containerPokemon = select("#container-pokemon");
 const Search = select("#Search");
 
-
-
-
-
 const renderPoke = async (id) => {
   containerPokemon.innerHTML = "<b>Cargando...</b>";
-  localStorage.removeItem(`Pokemon`)
-  if (id == 0 || null) {
-    alert("Insert a Valid Number")
-    localStorage.removeItem(`Pokemon`)
-    containerPokemon.innerHTML = "<b>Search a Pokemon!</b>";
+  localStorage.removeItem(`Pokemon`);
+  
+  if (id == 0 || id === "") {
+    containerPokemon.innerHTML = "<b>Insert a Valid ID!</b>";
     return;
   }
-  
-  if (id > 1025) {
-    alert("There is no more Pokemons")
-    localStorage.removeItem(`Pokemon`)
-    containerPokemon.innerHTML = "<b>Search a Pokemon!</b>";
-    return;
-  }
-  
+
   containerPokemon.innerHTML = "";
   
-  const pokemon = await getOne(id);
-  const { forms, sprites, abilities, height, weight, types} = pokemon;
-  containerPokemon.innerHTML=`
-  <article>
-  <picture>
-  <img src="${
-    sprites.other.home.front_default
-  }" alt="imagen de ${forms.map(nombre => nombre.name)}">
-  </picture>
-  <ul>
-  <h2>${forms.map(nombre => nombre.name)}</h2>
-  <li>Type/s <span>${types.map(tipo => tipo.type).map(nombre => nombre.name).join(' , ')}</span></li>
-  <li>Ability/Abilities <span>${abilities.map((ability) => ability.ability.name).join(" , ")}</span></li>
-  <li>Height: <span>${height / 10}/m</span></li>
-  <li>Weight: <span>${weight / 10} /Kg</span></li>
-  </ul>
-  </article>`;
-  save(`Pokemon`, pokemon.id);
+  try {
+    const pokemon = await getOne(id);
+    const { forms, sprites, abilities, height, weight, types } = pokemon;
+    containerPokemon.innerHTML = `
+      <article>
+        <picture>
+          <img src="${sprites.other.home.front_default}" alt="imagen de ${forms.map(nombre => nombre.name)}">
+        </picture>
+        <ul>
+          <h2>${forms.map(nombre => nombre.name)}</h2>
+          <li>Type/s <span>${types.map(tipo => tipo.type.name).join(', ')}</span></li>
+          <li>Ability/Abilities <span>${abilities.map((ability) => ability.ability.name).join(", ")}</span></li>
+          <li>Height: <span>${height / 10}/m</span></li>
+          <li>Weight: <span>${weight / 10} /Kg</span></li>
+        </ul>
+      </article>`;
+    
+    save(`Pokemon`, pokemon.id);
+  } catch (error) {
+    containerPokemon.innerHTML = "<b>Pokémon no encontrado. Intenta con otro ID.</b>";
+  }
 };
 
 Search.addEventListener("submit", (e) => {
